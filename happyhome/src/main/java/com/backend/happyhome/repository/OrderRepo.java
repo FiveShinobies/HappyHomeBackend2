c185822991b3ffffdef8a3872e4d8d374c8039df
Sahil <apturesahil2002@gmail.com>
2026-01-24 19:10:07 +0530

    Admin Vendor Details Implemented

--- happyhome/src/main/java/com/backend/happyhome/repository/OrderRepo.java
+++ happyhome/src/main/java/com/backend/happyhome/repository/OrderRepo.java
@@ -1,9 +1,13 @@
 package com.backend.happyhome.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.happyhome.entities.Order;
 
 public interface OrderRepo extends JpaRepository<Order, Long> {
 	long countByMyVendorVendorId(Long vendorId);
+	
+	List<Order> findByMyVendorVendorId(Long vendorId);
 }
