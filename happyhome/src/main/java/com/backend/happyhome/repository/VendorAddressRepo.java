b87077c1e313da21eb0b33c6ac801351bdeef69b
siddarth shankar singh <siddarthsingh421@gmail.com>
2026-01-27 04:06:05 +0530

    asdas

--- happyhome/src/main/java/com/backend/happyhome/repository/VendorAddressRepo.java
+++ happyhome/src/main/java/com/backend/happyhome/repository/VendorAddressRepo.java
@@ -5,7 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.happyhome.entities.Address;
+import com.backend.happyhome.entities.Vendor;
 
 public interface VendorAddressRepo extends JpaRepository<Address, Long> {
-	Optional<Address> findByMyUserUserId(Long userId);
+	Optional<Address> findByMyUserUserId(Long long1);
 }
